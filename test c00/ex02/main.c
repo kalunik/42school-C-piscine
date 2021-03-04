2c0babf472104fbb127d9b135f32a3e88394c860
kalunik <nikitaex123@gmail.com>
2021-03-04 19:11:42 +0300

    All stages

---
+++ test c00/ex02/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjonatho <wjonatho@student.21-schoo>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 19:20:05 by wjonatho          #+#    #+#             */
+/*   Updated: 2021/01/20 19:39:08 by wjonatho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+int		main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
